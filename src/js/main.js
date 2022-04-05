98890ab249853bf782fe886d599e0507488b1914
Rômulo Oliveira <romuloffall@gmail.com>
2022-04-05 15:10:04 -0300

    Acrescentei a galeria de imagens e preparei o footer

--- src/js/main.js
+++ src/js/main.js
@@ -5,8 +5,31 @@
 /*=============== SWIPER ===============*/
 
 
+let swiper = new Swiper(".mySwiper", {
+    loop: true,
+    spaceBetween: 10,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesProgress: true,
+  });
+  let swiper2 = new Swiper(".mySwiper2", {
+    loop: true,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+      swiper: swiper,
+    },
+    autoplay: {
+        delay: 3000,
+    },
+    disableOnInteraction: true,
+    mousewheel: {
+        invert: false,
+      },
+  });
 /*=============== SHOW SCROLL UP ===============*/ 
-
-
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
