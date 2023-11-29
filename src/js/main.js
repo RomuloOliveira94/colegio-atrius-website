50b6a34767d179ee18a142d8e68f8417f46ac03e
Rômulo Oliveira DEV <romuloffall@gmail.com>
2023-11-29 19:21:47 -0300

    #teste

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,14 @@
+
 /*=============== EFEITO DE SCROLL ===============*/
 const menuItems = document.querySelectorAll('.nav-list a[href^="#"]');
 console.log(menuItems);
 
-menuItems.forEach((item) => {
-  item.addEventListener("click", scrollToIdOnClick);
-});
+menuItems.forEach(item => {
+  item.addEventListener('click', scrollToIdOnClick);
+})
 
 function getScrollTopByHref(element) {
-  const id = element.getAttribute("href");
+  const id = element.getAttribute('href');
   return document.querySelector(id).offsetTop;
 }
 
@@ -38,13 +39,12 @@ function smoothScrollTo(endX, endY, duration) {
   const distanceY = endY - startY;
   const startTime = new Date().getTime();
 
-  duration = typeof duration !== "undefined" ? duration : 600;
+  duration = typeof duration !== 'undefined' ? duration : 600;
 
   // Easing function
   const easeInOutQuart = (time, from, distance, duration) => {
-    if ((time /= duration / 2) < 1)
-      return (distance / 2) * time * time * time * time + from;
-    return (-distance / 2) * ((time -= 2) * time * time * time - 2) + from;
+    if ((time /= duration / 2) < 1) return distance / 2 * time * time * time * time + from;
+    return -distance / 2 * ((time -= 2) * time * time * time - 2) + from;
   };
 
   const timer = setInterval(() => {
@@ -56,83 +56,43 @@ function smoothScrollTo(endX, endY, duration) {
     }
     window.scroll(newX, newY);
   }, 1000 / 60); // 60 fps
-}
+};
 
 /*=============== GALERIA ===============*/
 
 let swiper = new Swiper(".mySwiper", {
-  loop: true,
-  spaceBetween: 10,
-  slidesPerView: 4,
-  freeMode: true,
-  watchSlidesProgress: true,
-});
-let swiper2 = new Swiper(".mySwiper2", {
-  loop: true,
-  spaceBetween: 10,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  thumbs: {
-    swiper: swiper,
-  },
-  autoplay: {
-    delay: 3000,
-  },
-  disableOnInteraction: true,
-  mousewheel: {
-    invert: false,
-  },
-});
+    loop: true,
+    spaceBetween: 10,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesProgress: true,
+  });
+  let swiper2 = new Swiper(".mySwiper2", {
+    loop: true,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+      swiper: swiper,
+    },
+    autoplay: {
+        delay: 3000,
+    },
+    disableOnInteraction: true,
+    mousewheel: {
+        invert: false,
+      },
+  });
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
-ScrollReveal({ distance: "80%" });
-ScrollReveal().reveal(".text-banner", {
-  origin: "left",
-  delay: 500,
-  duration: 1000,
-  reset: true,
-});
-ScrollReveal().reveal(".banner-img", {
-  origin: "right",
-  delay: 500,
-  duration: 1000,
-  reset: true,
-});
-ScrollReveal().reveal(".banner-sobre", {
-  origin: "left",
-  delay: 500,
-  duration: 1000,
-  reset: true,
-});
-ScrollReveal().reveal(".paragrafos-sobre", {
-  origin: "right",
-  delay: 500,
-  duration: 1000,
-  reset: true,
-});
-ScrollReveal().reveal(".dif-01", {
-  origin: "right",
-  delay: 500,
-  duration: 800,
-  reset: true,
-});
-ScrollReveal().reveal(".dif-02", {
-  origin: "right",
-  delay: 1000,
-  duration: 800,
-  reset: true,
-});
-ScrollReveal().reveal(".dif-03", {
-  origin: "right",
-  delay: 1500,
-  duration: 800,
-  reset: true,
-});
-ScrollReveal().reveal(".footer-dif", {
-  origin: "bottom",
-  delay: 1800,
-  duration: 800,
-  reset: true,
-});
+ScrollReveal({ distance: '20%' });
+ScrollReveal().reveal('.text-banner', { origin: 'left', delay: 400, duration: 800});
+ScrollReveal().reveal('.banner-img', { origin: 'right', delay: 400, duration: 800});
+ScrollReveal().reveal('.banner-sobre', { origin: 'left', delay: 400, duration: 800});
+ScrollReveal().reveal('.paragrafos-sobre', { origin: 'right', delay: 400, duration: 800});
+ScrollReveal().reveal('.dif-01', { origin: 'right', delay: 400, duration: 800});
+ScrollReveal().reveal('.dif-02', { origin: 'right', delay: 700, duration: 800});
+ScrollReveal().reveal('.dif-03', { origin: 'right', delay: 1000, duration: 800});
+ScrollReveal().reveal('.footer-dif', { origin: 'bottom', delay: 400, duration: 800});
