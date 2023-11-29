3662ed04359a087fdb46d7642754478ea9320c99
Rômulo Oliveira DEV <romuloffall@gmail.com>
2023-11-29 19:20:31 -0300

    update site

---
+++ src/js/main.js
@@ -0,0 +1,98 @@
+
+/*=============== EFEITO DE SCROLL ===============*/
+const menuItems = document.querySelectorAll('.nav-list a[href^="#"]');
+console.log(menuItems);
+
+menuItems.forEach(item => {
+  item.addEventListener('click', scrollToIdOnClick);
+})
+
+function getScrollTopByHref(element) {
+  const id = element.getAttribute('href');
+  return document.querySelector(id).offsetTop;
+}
+
+function scrollToIdOnClick(event) {
+  event.preventDefault();
+  const to = getScrollTopByHref(event.target) + 20;
+  scrollToPosition(to);
+}
+
+function scrollToPosition(to) {
+  // window.scroll({
+  //   top: to,
+  //   behavior: "smooth",
+  // });
+  smoothScrollTo(0, to);
+}
+
+/**
+ * Smooth scroll animation
+ * @param {int} endX: destination x coordinate
+ * @param {int} endY: destination y coordinate
+ * @param {int} duration: animation duration in ms
+ */
+function smoothScrollTo(endX, endY, duration) {
+  const startX = window.scrollX || window.pageXOffset;
+  const startY = window.scrollY || window.pageYOffset;
+  const distanceX = endX - startX;
+  const distanceY = endY - startY;
+  const startTime = new Date().getTime();
+
+  duration = typeof duration !== 'undefined' ? duration : 600;
+
+  // Easing function
+  const easeInOutQuart = (time, from, distance, duration) => {
+    if ((time /= duration / 2) < 1) return distance / 2 * time * time * time * time + from;
+    return -distance / 2 * ((time -= 2) * time * time * time - 2) + from;
+  };
+
+  const timer = setInterval(() => {
+    const time = new Date().getTime() - startTime;
+    const newX = easeInOutQuart(time, startX, distanceX, duration);
+    const newY = easeInOutQuart(time, startY, distanceY, duration);
+    if (time >= duration) {
+      clearInterval(timer);
+    }
+    window.scroll(newX, newY);
+  }, 1000 / 60); // 60 fps
+};
+
+/*=============== GALERIA ===============*/
+
+let swiper = new Swiper(".mySwiper", {
+    loop: true,
+    spaceBetween: 10,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesProgress: true,
+  });
+  let swiper2 = new Swiper(".mySwiper2", {
+    loop: true,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+      swiper: swiper,
+    },
+    autoplay: {
+        delay: 3000,
+    },
+    disableOnInteraction: true,
+    mousewheel: {
+        invert: false,
+      },
+  });
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+ScrollReveal({ distance: '20%' });
+ScrollReveal().reveal('.text-banner', { origin: 'left', delay: 400, duration: 800});
+ScrollReveal().reveal('.banner-img', { origin: 'right', delay: 400, duration: 800});
+ScrollReveal().reveal('.banner-sobre', { origin: 'left', delay: 400, duration: 800});
+ScrollReveal().reveal('.paragrafos-sobre', { origin: 'right', delay: 400, duration: 800});
+ScrollReveal().reveal('.dif-01', { origin: 'right', delay: 400, duration: 800});
+ScrollReveal().reveal('.dif-02', { origin: 'right', delay: 700, duration: 800});
+ScrollReveal().reveal('.dif-03', { origin: 'right', delay: 1000, duration: 800});
+ScrollReveal().reveal('.footer-dif', { origin: 'bottom', delay: 400, duration: 800});
---
+++ src/js/whats-app-form.js
@@ -0,0 +1,74 @@
+
+const phoneNumber = "5585981337203"
+
+function submitForm() {
+    document.querySelector('.form-matricula').addEventListener('submit', getFormData)
+}
+
+function getFormData(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target)
+    const nomeResp = formData.get('nome_resp')
+    const cpf = formData.get('cpf')
+    const enderecoRua = formData.get('endereco_rua')
+    const enderecoNumero = formData.get('endereco_numero')
+    const enderecoBairro = formData.get('endereco_bairro')
+    const nomeAluno = formData.get('nome_aluno')
+    const serie = formData.get('serie')
+    const turno = formData.get('turno')
+    const obs = formData.get('obs')
+
+    if(!nomeResp || !cpf || !enderecoRua || !enderecoNumero || !enderecoBairro || !nomeAluno || !serie || !turno ) {
+        document.querySelector('.error').innerText = 'Preencha todos os campos obrigatórios'
+        return
+    }
+
+
+    const data =  {
+        nomeResp,
+        cpf,
+        enderecoRua,
+        enderecoNumero,
+        enderecoBairro,
+        nomeAluno,
+        serie,
+        turno,
+        pagamento,
+        obs
+    }
+
+    return sendToZap(data)
+}
+
+function sendToZap(data) {
+
+    const textTemplate = `*REALIZEI MINHA PRÉ-MATRICULA PELO SITE!*
+
+    *Dados*
+    
+    Nome do Responsável: *${data.nomeResp}*
+    Cpf: *55522255584*
+    Endereço: *${data.enderecoRua}, ${data.enderecoNumero} - ${data.enderecoBairro}*
+    Nome do aluno: *${data.nomeAluno}*
+    Série: *${data.serie}*
+    Turno: *${data.turno}*
+    Forma de Pagamento: *${data.pagamento}*
+    Observações: *${data.obs}*
+    
+    *Obs.: Comparecer ao colégio com as seguintes documentações:*
+    RG e CPF (Responsáveis)
+    Certidão de nascimento (aluno)
+    Comprovante de Endereço
+    Foto 3x4 (aluno)
+    Declaração de Transferência e/ou Histórico escolar
+    Pasta escolar
+    `
+
+    const encondeText = encodeURIComponent(textTemplate)
+
+    console.log(encondeText);
+
+    const whatsAppLink = `https://wa.me//${phoneNumber}?text=${encondeText}`
+
+    window.location.href = whatsAppLink;
+}
